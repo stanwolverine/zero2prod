4ce284f8de746eb6271833573c2aa658a27f0f29
Jatin Nagar <jitu19nagar@gmail.com>
2024-04-27 14:55:28 +0530

    hello world http server changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world! This is Jatin :)");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello World!"
+}
+
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello, {name}")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    const PORT: u16 = 8081;
+
+    HttpServer::new(|| App::new().service(index).service(hello))
+        .bind(("localhost", PORT))?
+        .run()
+        .await
 }
